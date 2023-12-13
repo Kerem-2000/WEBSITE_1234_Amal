96def7f597d8446f545df640fe2e0275bedad396
Kerem-2000 <153702841+Kerem-2000@users.noreply.github.com>
2023-12-13 10:39:13 +0400

    Update check.py

--- check.py
+++ check.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Amal's love", page_icon=":tada:", layout="wide")
 
-st.subheader("KorAmlaaaaaaaal")
-st.title("Do you want to learn who is Amal's crush")
+
+st.title("Do you want to learn who is KorAmaaal's crush?")
 
 button_clicked = st.button("Click to see")
 
